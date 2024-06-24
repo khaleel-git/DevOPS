11865815859a1bc21204c673d220b5b59a6b9c02
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-24 17:17:01 +0500

    2024-06-24 17:17:01

--- ShellScript/substr.sh
+++ ShellScript/substr.sh
@@ -10,8 +10,8 @@ echo "${str:3:3}"
 echo "${str: -5}" # with space    -> print 5 characters starting from last character
 echo "${str:-5}"  # without space -> don't perform substr operation
 
-echo "${str#a*c}"  #  starting, shortest match
-echo "${str##a*c}" #  starting, longest match
+echo "${str#a*c}"  #  starting, shortest match -> remove a to c, then shortest match print
+echo "${str##a*c}" #  starting, longest match  -> remove a to c, then longest match print
 
 echo ${str%b*z}    #  ending, shortest match
 echo "${str%%b*z}" #  ending, longest match
