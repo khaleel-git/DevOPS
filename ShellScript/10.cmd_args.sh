f39420cf4fdc9e7535994f3f59f940a3a39abaed
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-24 16:17:01 +0500

    2024-06-24 16:17:01

--- ShellScript/10.cmd_args.sh
+++ ShellScript/10.cmd_args.sh
@@ -6,6 +6,23 @@ echo ${0}
 echo ${4}
 echo ${5}
 echo "my name is ${name}, and my age is ${age}, and my height is ${height}"
+
+echo "printing output of $#"
 echo $# # prints only passed args
+echo -e "\n"
+
+echo "printing output of $@"
 echo $@ # prints all args as a separate words
+echo -e "\n"
+
+echo "printing output of ${@}"
+echo ${@}
+echo -e "\n"
+
+echo "printing output of $*"
 echo $* # prints all args as a single words
+echo -e "\n"
+
+echo "printing output of ${*}"
+echo ${*}
+echo -e "\n"
