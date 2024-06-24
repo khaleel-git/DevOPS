5f7985d528fedc6dc7b8f1802e1ead5bb055ecd0
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-24 15:41:01 +0500

    2024-06-24 15:41:01

--- ShellScript/9.input.sh
+++ ShellScript/9.input.sh
@@ -6,4 +6,8 @@ read name
 echo "Please Enter your age: "
 read age
 
+echo "Getting input without explicitly using assigning values to variables, in this case REPLAY variable will be used."
+read
+echo $REPLAY
+
 echo "My name is ${name} and my age is ${age}"
