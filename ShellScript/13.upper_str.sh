4b6927bd3830096cca38046d5a91410678dea17b
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-24 16:56:01 +0500

    2024-06-24 16:56:01

--- ShellScript/13.upper_str.sh
+++ ShellScript/13.upper_str.sh
@@ -1,6 +1,12 @@
 #!/bin/zsh
 
+echo "Converting str into Uppercase"
 str="my name is khaleel"
 echo "${str}"
-echo "${string^}" # only uppercase first letter
+echo "${str^}"    # only uppercase first letter
 echo "${str^^}"   # convert whole str into uppercase
+
+echo "Converting str into lowercase"
+str = "KHALEEL AHMAD"
+echo "${str,}"  # only first letter
+echo "${str,,}" # convert completley
