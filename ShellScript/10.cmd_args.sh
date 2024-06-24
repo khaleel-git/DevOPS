bc27697c9889d7d95e9f54c1a23f1a88cc4d0742
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-24 16:09:01 +0500

    2024-06-24 16:09:01

--- ShellScript/10.cmd_args.sh
+++ ShellScript/10.cmd_args.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 name=${1}
 age=${2}
-echo ${1}
+height=${3}
 echo ${2}
 echo ${3}
-echo "my name is ${name}, and my age is ${age}"
+echo "my name is ${name}, and my age is ${age}, and my height is ${height}"
 echo $#
 echo $@
 echo $*
