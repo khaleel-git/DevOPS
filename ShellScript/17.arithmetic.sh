6386d6e826d0119514877409bf67bf4e57df9ebb
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-26 13:45:02 +0500

    2024-06-26 13:45:02

--- ShellScript/17.arithmetic.sh
+++ ShellScript/17.arithmetic.sh
@@ -4,13 +4,18 @@ echo "Arithmetic using double parenthesis i.e (())"
 
 a=5
 b=6
-echo "$((a+b))"
-echo "$((a-b))"
-echo "$((a*b))"
-echo "$((a/b))" # 5/6
-echo "$((a%b))"
-echo "$((2**3))" # 2*2*2
+echo "a = $a, b = $b"
+
+echo "a+b = $((a+b))"
+echo "a-b = $((a-b))"
+echo "a*b = $((a*b))"
+
+res_div=$(echo "scale=2; $a / $b" | bc)
+echo "a/b = $res_div" # 5/6
+
+echo "a%b = $((a%b))"
+echo "2pow3 = $((2**3))" # 2*2*2
 ((a++)) # a=a+1
-echo $a
+echo "a++ or a=a+1 = $a"
 ((a+=3)) # a=a+3
-echo $a
+echo "a+=3 or a=a+3 = $a"
