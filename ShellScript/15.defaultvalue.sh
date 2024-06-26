b5251a6217a2ec3306ffca4c1e055f6a8abaf101
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-26 12:49:03 +0500

    2024-06-26 12:49:03

---
+++ ShellScript/15.defaultvalue.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p " please enter your name " name
+
+name=${name:-World}
+
+echo "Hello ${name^}"
+
+yourname=${unsetvariable-Ahmad}
+echo $yourname
+
+myname=""
+mytestname=${myname:-kali}
+echo ${mytestname}
