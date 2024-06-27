45ecb0684c15130d477e3d38b1eb0a3f6b469c07
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-27 15:24:33 +0500

    terraform commit @ 2024-06-27 15:24:33

--- git_push.sh
+++ git_push.sh
@@ -3,5 +3,5 @@ cd /home/khaleel/DevOPS
 #git fetch 
 #git pull
 git add .
-git commit -m "$(date +'%Y-%m-%d %H:%M:%S')"  
+git commit -m "terraform commit @ $(date +'%Y-%m-%d %H:%M:%S')"  
 git push -u origin master
