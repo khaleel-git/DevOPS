e89d6cfe2a3eaa9fa2cc08df983127a2ee1a62a3
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-28 16:53:01 +0500

    terraform commit @ 2024-06-28 16:53:01

--- terraform/9.terraform-aws/ec2-security-group/main.tf
+++ terraform/9.terraform-aws/ec2-security-group/main.tf
@@ -19,7 +19,13 @@ resource "aws_security_group" "terraformsecuritygroup" {
     }
   }
 
-  
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+  }
 }
 
 resource "aws_instance" "newvm" {
