ceff4c1167cece76cad835976504be6a0c63621d
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-28 13:10:01 +0500

    terraform commit @ 2024-06-28 13:10:01

---
+++ terraform/9.terraform-aws/simple_ec2/main.tf
@@ -0,0 +1,11 @@
+# default security group assigned
+# inbound: http, https, ssh
+
+resource "aws_instance" "web" {
+  ami           = "ami-04b70fa74e45c3917"
+  instance_type = "t2.micro"
+  key_name      = "aws-linux"
+  tags = {
+    Name = "web2-tfinstance"
+  }
+}
