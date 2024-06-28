1903bb81a276ce95cff1b8e0520d1944ff7e9406
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-28 16:47:01 +0500

    terraform commit @ 2024-06-28 16:47:01

--- terraform/9.terraform-aws/ec2-security-group/main.tf
+++ terraform/9.terraform-aws/ec2-security-group/main.tf
@@ -24,13 +24,9 @@ resource "aws_instance" "newvm" {
   ami             = "ami-04b70fa74e45c3917"
   instance_type   = "t2.micro"
   key_name        = aws_key_pair.terraform_key.key_name
-  vpc_security_gr = ["${aws_security_group.terraformsecuritygroup.id}"]
+  vpc_security_group_ids = [aws_security_group.terraformsecuritygroup.id]
 
   tags = {
     Name = "myvmtf"
   }
-}
-
-output "securitygroupdetails" {
-  value = aws_security_group.terraformsecuritygroup.id
 }
