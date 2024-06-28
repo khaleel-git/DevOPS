e94e328bfdca40ea7562b6effc9573b3fe1d5afc
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-28 15:00:01 +0500

    terraform commit @ 2024-06-28 15:00:01

--- terraform/9.terraform-aws/aws_keypair/providers.tf
+++ terraform/9.terraform-aws/aws_keypair/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 # configure aws
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIA6ODU2JJ4AX4M6WXZ"
-  secret_key = "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"
+#   region     = "us-east-1"
+#   access_key = ""
+#   secret_key = ""
 }
