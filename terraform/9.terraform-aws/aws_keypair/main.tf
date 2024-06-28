9e4f2e8c4fe94b35b7ff163360cd778c3c31c8ce
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-28 14:28:01 +0500

    terraform commit @ 2024-06-28 14:28:01

--- terraform/9.terraform-aws/aws_keypair/main.tf
+++ terraform/9.terraform-aws/aws_keypair/main.tf
@@ -2,3 +2,7 @@ resource "aws_key_pair" "terraform_key" {
   key_name   = "terraform_key"
   public_key = file("/home/khaleel/.ssh/aws_rsa.pub")
 }
+
+output "keypair" {
+  value = aws_key_pair.terraform_key.key_name
+}
