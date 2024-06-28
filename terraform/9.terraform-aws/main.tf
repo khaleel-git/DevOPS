81d71325442f907eac34b2283218b428a585404f
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-28 12:54:02 +0500

    terraform commit @ 2024-06-28 12:54:02

--- terraform/9.terraform-aws/main.tf
+++ terraform/9.terraform-aws/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
-  ami = "ami-04b70fa74e45c3917"
+  ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
-
+  key_name      = "aws-linux"
   tags = {
-    name = "web2"
+    name = "web2-tfinstance"
   }
 }
