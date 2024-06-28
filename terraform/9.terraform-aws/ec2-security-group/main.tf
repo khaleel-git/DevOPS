8327799785adfc707c20d85d6657252b3f37ffff
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-28 15:25:01 +0500

    terraform commit @ 2024-06-28 15:25:01

--- terraform/9.terraform-aws/ec2-security-group/main.tf
+++ terraform/9.terraform-aws/ec2-security-group/main.tf
@@ -13,17 +13,46 @@ resource "aws_instance" "newvm" {
 }
 
 resource "aws_security_group" "terraformsecuritygroup" {
-  name = " allow_tls"
+  name        = "allow_tls"
   description = "Allow TLS inbound traffic"
 
-  ingress = {
-    description = "TLS from VPC"
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+  // Ingress rule for SSH (port 22)
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // Ingress rule for HTTP (port 80)
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // Ingress rule for HTTPS (port 443)
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // Ingress rule for MongoDB (port 27017)
+  ingress {
+    description = "MongoDB from VPC"
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 
 output "keypair" {
   value = aws_key_pair.terraform_key.key_name
