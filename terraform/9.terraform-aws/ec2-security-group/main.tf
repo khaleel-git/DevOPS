24b0531072f21ff0d695415206489e06bc60c63e
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-28 16:28:01 +0500

    terraform commit @ 2024-06-28 16:28:01

--- terraform/9.terraform-aws/ec2-security-group/main.tf
+++ terraform/9.terraform-aws/ec2-security-group/main.tf
@@ -3,53 +3,17 @@ resource "aws_key_pair" "terraform_key" {
   public_key = file("/home/khaleel/.ssh/aws_rsa.pub")
 }
 
- dynamic "setting" {
-    for_each = var.settings
+resource "aws_security_group" "terraformsecuritygroup" {
+  name        = "mysecuritygrouptf"
+  description = "Allow TLS inbound traffic"
+
+  dynamic "ingress" {
+    for_each = [22,]
     content {
       namespace = setting.value["namespace"]
       name = setting.value["name"]
       value = setting.value["value"]
     }
-  }
-
-resource "aws_security_group" "terraformsecuritygroup" {
-  name        = "mysecuritygrouptf"
-  description = "Allow TLS inbound traffic"
-
-  // Ingress rule for SSH (port 22)
-  ingress {
-    description = "SSH from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  // Ingress rule for HTTP (port 80)
-  ingress {
-    description = "HTTP from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  // Ingress rule for HTTPS (port 443)
-  ingress {
-    description = "HTTPS from VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  // Ingress rule for MongoDB (port 27017)
-  ingress {
-    description = "MongoDB from VPC"
-    from_port   = 27017
-    to_port     = 27017
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
