c47d16827c70ea7229f3c8980b66bc57b6104511
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-28 14:26:01 +0500

    terraform commit @ 2024-06-28 14:26:01

--- terraform/9.terraform-aws/aws_keypair/providers.tf
+++ terraform/9.terraform-aws/aws_keypair/providers.tf
@@ -1,7 +1,13 @@
 terraform {
     required_providers {
       aws = {
-        
+        source = "hashicorp/aws"
+        version = "~> 5.0"
       }
     }
+}
+
+# configure aws
+provider "aws" {
+  region = 
 }
