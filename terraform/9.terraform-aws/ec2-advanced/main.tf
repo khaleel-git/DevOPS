88d71c52ae53b66de8422440fceefade8b846310
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-28 15:07:02 +0500

    terraform commit @ 2024-06-28 15:07:02

--- terraform/9.terraform-aws/ec2-advanced/main.tf
+++ terraform/9.terraform-aws/ec2-advanced/main.tf
@@ -1,3 +1,8 @@
+resource "aws_key_pair" "terraform_key" {
+  key_name   = "terraform_key"
+  public_key = file("/home/khaleel/.ssh/aws_rsa.pub")
+}
+
 resource "aws_instance" "newvm" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
@@ -7,6 +12,10 @@ resource "aws_instance" "newvm" {
   }
 }
 
+
+output "keypair" {
+  value = aws_key_pair.terraform_key.key_name
+}
 output "ip" {
   value = aws_instance.newvm.public_ip
 }
