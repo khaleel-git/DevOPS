e6c39a72cd3cd044b33bb40328e2fee894156430
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-28 14:36:01 +0500

    terraform commit @ 2024-06-28 14:36:01

--- terraform/9.terraform-aws/ec2-security-group/main.tf
+++ terraform/9.terraform-aws/ec2-security-group/main.tf
@@ -1,11 +1,11 @@
 # default security group assigned
 # inbound: http, https, ssh
 
-resource "aws_instance" "web" {
+resource "aws_instance" "newvm" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
-  key_name      = "aws-linux"
+  key_name      = "terraform_key"
   tags = {
-    Name = "terraform_key"
+    Name = "terraform_key_Attached"
   }
 }
