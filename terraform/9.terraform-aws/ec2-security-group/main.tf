ffd915ade174e2fbe6486cf3f2945854203e55fd
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-28 15:45:01 +0500

    terraform commit @ 2024-06-28 15:45:01

--- terraform/9.terraform-aws/ec2-security-group/main.tf
+++ terraform/9.terraform-aws/ec2-security-group/main.tf
@@ -4,16 +4,16 @@ module "modules" {
 }
 
 resource "aws_key_pair" "terraform_key" {
-  key_name   = "terraform_key"
+  key_name   = "mykeytf"
   public_key = file("/home/khaleel/.ssh/aws_rsa.pub")
 }
 
 resource "aws_instance" "newvm" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
-  key_name      = "terraform_key"
+  key_name      = "mykeytf"
   # security_group = aws_security_group.terraformsecuritygroup.id
   tags = {
-    Name = "terraform_key_Attached"
+    Name = "myvmtf"
   }
 }
