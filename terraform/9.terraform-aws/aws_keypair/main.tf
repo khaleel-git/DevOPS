4bf7a7bea6fc066ad0d0d8701bd019e8fee37297
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-28 14:27:01 +0500

    terraform commit @ 2024-06-28 14:27:01

--- terraform/9.terraform-aws/aws_keypair/main.tf
+++ terraform/9.terraform-aws/aws_keypair/main.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "terraform_key" {
-  key_name = "terraform_key"
+  key_name   = "terraform_key"
   public_key = file("/home/khaleel/.ssh/aws_rsa.pub")
 }
--- terraform/9.terraform-aws/aws_keypair/providers.tf
+++ terraform/9.terraform-aws/aws_keypair/providers.tf
@@ -1,13 +1,15 @@
 terraform {
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
 
 # configure aws
 provider "aws" {
-  region = 
+  region     = "us-east-1"
+  access_key = "AKIA6ODU2JJ4AX4M6WXZ"
+  secret_key = "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"
 }
