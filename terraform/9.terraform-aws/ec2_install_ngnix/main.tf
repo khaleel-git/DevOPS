386f43fb41861fa92516e7b1f2adce88a67ab59a
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-29 14:56:01 +0500

    terraform commit @ 2024-06-29 14:56:01

--- terraform/9.terraform-aws/ec2_install_ngnix/main.tf
+++ terraform/9.terraform-aws/ec2_install_ngnix/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "newvm" {
 ls > ls.txt
 sudo apt-get update -y
 sudo apt-get install nginx -y
-sudo echo "Hello Nginx
+sudo echo "Hello Nginx" > /var/www/html/index.nginx-debian.html
 EOF
 
   tags = {
