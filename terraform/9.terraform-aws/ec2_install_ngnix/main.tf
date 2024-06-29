af5b0dd17d89fd6d1cca7a783c2672337def2a41
Khaleel Ahmad <khaleel.org@gmail.com>
2024-06-29 15:12:01 +0500

    terraform commit @ 2024-06-29 15:12:01

--- terraform/9.terraform-aws/ec2_install_ngnix/main.tf
+++ terraform/9.terraform-aws/ec2_install_ngnix/main.tf
@@ -4,13 +4,7 @@ resource "aws_instance" "newvm" {
   key_name               = aws_key_pair.terraform_key.key_name
   vpc_security_group_ids = [aws_security_group.terraformsecuritygroup.id]
 
-user_data = <<-EOF
-  #!/bin/bash
-  ls > ls.txt
-  sudo apt-get update -y
-  sudo apt-get install nginx -y
-  sudo echo "Hello Nginx" > /var/www/html/index.nginx-debian.html
-EOF
+user_data = 
 
   tags = {
     Name = "newnametf"
