09f41e1be25a39011b748f3bcdff1a40cb051d40
Khaleel Ahmad <khaleel.org@gmail.com>
2024-07-01 18:20:00 +0500

    terraform commit @ 2024-07-01 18:20:00

--- terraform/11.aws-data_sources/main.tf
+++ terraform/11.aws-data_sources/main.tf
@@ -1,25 +1,25 @@
-resource "aws_instance" "newvm" {
-  ami                    = var.image_id
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.terraform_key.key_name
-  vpc_security_group_ids = [aws_security_group.terraformsecuritygroup.id]
+# resource "aws_instance" "newvm" {
+#   ami                    = var.image_id
+#   instance_type          = var.instance_type
+#   key_name               = aws_key_pair.terraform_key.key_name
+#   vpc_security_group_ids = [aws_security_group.terraformsecuritygroup.id]
 
-  tags = {
-    Name = "newnametflocalprovisioner"
-  }
-}
+#   tags = {
+#     Name = "newnametflocalprovisioner"
+#   }
+# }
 
-output "publicip" {
-  value = aws_instance.newvm.public_ip
-}
+# output "publicip" {
+#   value = aws_instance.newvm.public_ip
+# }
 
 data "aws_ami" "ami" {
   most_recent = true
-  owners = []
+  owners = ["099720109477"]
 
     filter {
     name   = "name"
-    values = ["myami-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
@@ -32,3 +32,4 @@ data "aws_ami" "ami" {
     values = ["hvm"]
   }
 }
+
