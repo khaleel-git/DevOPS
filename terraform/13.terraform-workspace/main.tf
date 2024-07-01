97f3419ea9776dd21a44b3d38e457484d96f37c3
Khaleel Ahmad <khaleel.org@gmail.com>
2024-07-01 20:07:01 +0500

    terraform commit @ 2024-07-01 20:07:01

---
+++ terraform/13.terraform-workspace/main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "newvm" {
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.terraform_key.key_name
+  vpc_security_group_ids = [aws_security_group.terraformsecuritygroup.id]
+
+  tags = {
+    Name = "newnametflocalprovisioner"
+  }
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.ami.id
+}
+
+output "public_ip" {
+  value = aws_instance.newvm.public_ip
+}
+
