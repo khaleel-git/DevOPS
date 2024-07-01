4ab785d2205286d465bd7be2c063134ff78d2a58
Khaleel Ahmad <khaleel.org@gmail.com>
2024-07-01 15:30:01 +0500

    terraform commit @ 2024-07-01 15:30:01

--- terraform/10.provisioners/remote-exec/nginx.sh
+++ terraform/10.provisioners/remote-exec/nginx.sh
@@ -2,5 +2,4 @@
 ls > ls.txt
 sudo apt-get update -y
 sudo apt-get install nginx -y
-sudo rm /var/www/html/index.nginx-debian.html
-sudo echo "Hello Nginx" > /var/www/html/index.nginx-debian.html
+echo "Hello Nginx" | sudo tee /var/www/html/index.nginx-debian.html
