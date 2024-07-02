4453913e7faa948495b701500aae8a528c1e91b4
Khaleel Ahmad <khaleel.org@gmail.com>
2024-07-02 13:20:02 +0500

    terraform commit @ 2024-07-02 13:20:02

--- terraform/14.terraform-module/modules/web-server/resource.tf
+++ terraform/14.terraform-module/modules/web-server/resource.tf
@@ -2,9 +2,10 @@
 
 resource "aws_instance" "newvm" {
   ami                    = data.aws_ami.ami.id
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.terraform_key.key_name
-  vpc_security_group_ids = [aws_security_group.terraformsecuritygroup.id]
+  # instance_type          = var.instance_type
+  instance_type          = "t2.micro"
+  # key_name               = aws_key_pair.terraform_key.key_name
+  # vpc_security_group_ids = [aws_security_group.terraformsecuritygroup.id]
 
   tags = {
     Name = "newnametflocalprovisioner"
@@ -17,4 +18,24 @@ output "aws_ami_id" {
 
 output "public_ip" {
   value = aws_instance.newvm.public_ip
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
