df45a8898d0ab663c1c285e145369ce2ca59ba12
Khaleel Ahmad <khaleel.org@gmail.com>
2024-07-02 14:55:02 +0500

    terraform commit @ 2024-07-02 14:55:02

--- terraform/15.terraform-backend/providers.tf
+++ terraform/15.terraform-backend/providers.tf
@@ -1,7 +1,33 @@
 terraform {
   required_providers {
     aws = {
-        source = "ha"
+        source = "hashicorp/aws"
+        version = "~> 5.0"
     }
+  }
+}
+
+provider "aws" {
+  
+}
+
+
+data "aws_ami" "ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
