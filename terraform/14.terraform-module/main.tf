43c5d96ba8fe4b0763774c322dc828562ddebb2a
Khaleel Ahmad <khaleel.org@gmail.com>
2024-07-02 14:10:04 +0500

    terraform commit @ 2024-07-02 14:10:04

--- terraform/14.terraform-module/main.tf
+++ terraform/14.terraform-module/main.tf
@@ -2,4 +2,6 @@ module "web-server" {
   source        = "./modules/web-server"
   instance_type = "t2.micro"
   ports         = [22, 80, 8080, 443, 3306, 27017]
+  key_name   = "mykeytf"
+  public_key = file("/home/khaleel/.ssh/aws_rsa.pub")
 }
