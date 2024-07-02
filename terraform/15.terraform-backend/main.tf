fbc1d18274905752ec408b1002873be19906efea
Khaleel Ahmad <khaleel.org@gmail.com>
2024-07-02 15:08:02 +0500

    terraform commit @ 2024-07-02 15:08:02

--- terraform/15.terraform-backend/main.tf
+++ terraform/15.terraform-backend/main.tf
@@ -8,7 +8,7 @@ terraform {
 
 resource "aws_instance" "newvm" {
   ami                    = data.aws_ami.ami.id
-  instance_type          = "t2.micro"
+  instance_type          = "t2.small"
   key_name               = aws_key_pair.terraform_key.key_name
   vpc_security_group_ids = [aws_security_group.terraformsecuritygroup.id]
 
