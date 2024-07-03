43da39f754f9908eb527b34f8682bcae2895e43b
Khaleel Ahmad <khaleel.org@gmail.com>
2024-07-03 10:09:02 +0500

    terraform commit @ 2024-07-03 10:09:02

--- terraform/4.terraform.tfvars/main.tf
+++ terraform/4.terraform.tfvars/main.tf
@@ -1,9 +1,10 @@
 output "userage" {
   value = "my name is ${var.username} and my age is ${lookup(var.userage,var.username)}"
 }
+## below is invalid -> wrong order lookup function arguments
+# output "demo" {
+#   value = "${lookup(var.username,var.userage)}"
+# }
 
-output "demo" {
-  value = "${lookup(var.username)}"
-}
-
+# below line pass map in 
 # terraform plan -var 'usersage={ "gaurav":"23","saurav":"22","men":"25" }'
