97c380e4dd249ca09e48f28ab3c51fe0750795a7
Khaleel Ahmad <khaleel.org@gmail.com>
2024-07-03 10:08:04 +0500

    terraform commit @ 2024-07-03 10:08:04

--- terraform/4.terraform.tfvars/main.tf
+++ terraform/4.terraform.tfvars/main.tf
@@ -2,6 +2,8 @@ output "userage" {
   value = "my name is ${var.username} and my age is ${lookup(var.userage,var.username)}"
 }
 
-
+output "demo" {
+  value = "${lookup(var.username)}"
+}
 
 # terraform plan -var 'usersage={ "gaurav":"23","saurav":"22","men":"25" }'
