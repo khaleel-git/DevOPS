29c5dc6850c00b6aca39ffdcae56b0670652cc76
Khaleel Ahmad <khaleel.org@gmail.com>
2024-08-06 22:24:01 +0500

    Ansible Commit @ 2024-08-06 22:24:010

--- Ansible/10.Ansible-Projects/Ansible_Shell_Scripts/host_details.sh
+++ Ansible/10.Ansible-Projects/Ansible_Shell_Scripts/host_details.sh
@@ -1,2 +1,2 @@
-ANSIBLE_HOST_KEY_CHECKING=False ansible -a 'cat /etc/hosts' -i inventory all
+ANSIBLE_HOST_KEY_CHECKING=False ansible -a 'hostname' -i inventory all
 ansible -m copy -a 'src=/etc/resolv.conf dest=/tmp/resolv.conf' -i inventory node00
