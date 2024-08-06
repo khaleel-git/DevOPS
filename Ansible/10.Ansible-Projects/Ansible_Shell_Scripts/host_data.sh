226dfcca4f1514a38b420183fb0725b58524ce0a
Khaleel Ahmad <khaleel.org@gmail.com>
2024-08-06 23:06:01 +0500

    Ansible Commit @ 2024-08-06 23:06:010

--- Ansible/10.Ansible-Projects/Ansible_Shell_Scripts/host_data.sh
+++ Ansible/10.Ansible-Projects/Ansible_Shell_Scripts/host_data.sh
@@ -1,2 +1,18 @@
+# Set Ansible gathering to explicit to avoid automatic fact gathering
 export ANSIBLE_GATHERING=explicit
+
+# Run an ad-hoc Ansible command to check the uptime of all hosts in the inventory
 ansible -a 'uptime' -i inventory all
+
+# Create a playbook to check the Red Hat release version
+echo '''
+---
+- hosts: all
+  become: true
+  tasks:
+    - name: Check Red Hat release
+      command: cat /etc/redhat-release
+''' > playbook.yml
+
+# Run playbook
+ansible-playbook -i inventory -vv
