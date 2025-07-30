446844ad940685576094cf306c595a4e34c93207
Khaleel Ahmad <khaleel.org@gmail.com>
2025-07-30 17:00:01 +0200

    Commit @ 2025-07-30 17:00:01

--- Web-Projects/1.DevOpsified/go-web-app/terraform/main.tf
+++ Web-Projects/1.DevOpsified/go-web-app/terraform/main.tf
@@ -16,30 +16,50 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version         = "20.8.4"
-  cluster_name    = local.cluster_name
-  cluster_version = var.kubernetes_version
+  version = "~> 21.0"
 
-  subnet_ids      = module.vpc.public_subnets
-  vpc_id          = module.vpc.vpc_id
+  name               = var.cluster_name
+  kubernetes_version = "1.30"
 
+  addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy = {}
+    vpc-cni    = {
+      before_compute = true
+    }
+  }
+  
   enable_irsa = true
+  endpoint_public_access                   = true
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.public_subnets
+  control_plane_subnet_ids = module.vpc.public_subnets
 
   eks_managed_node_groups = {
     public-nodes = {
+      ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["t3.small"]
-      min_size       = 1
-      max_size       = 2
-      desired_size   = 1
-      subnet_ids     = module.vpc.public_subnets
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      subnet_ids = module.vpc.public_subnets
     }
   }
 
   tags = {
-    environment = "dev"
-    project     = "go-web-app"
+    Environment = "dev"
+    Terraform   = "true"
+    Project     = "go-web-app"
   }
 }
+
 
 
 
